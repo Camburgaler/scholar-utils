75efb4b9071abd62d389d9a571173964e15bc437
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-07-09 14:04:46 -0500

    moved constants and enums out from transform.go

---
+++ pkg/transform/constants.go
@@ -0,0 +1,127 @@
+package transform
+
+import "github.com/Camburgaler/scholar-utils/pkg/output"
+
+var (
+	noHelmet = output.Armor{
+		Name: "No Helmet",
+		Defenses: output.Defenses{
+			Slash:    0,
+			Thrust:   0,
+			Strike:   0,
+			Standard: 0,
+		},
+		Absorptions: output.Absorptions{
+			Magic:     0,
+			Lightning: 0,
+			Fire:      0,
+			Dark:      0,
+			Poison:    0,
+			Bleed:     0,
+			Petrify:   0,
+			Curse:     0,
+		},
+		Poise:  0,
+		Weight: 0,
+	}
+	noChestpiece = output.Armor{
+		Name: "No Chestpiece",
+		Defenses: output.Defenses{
+			Slash:    0,
+			Thrust:   0,
+			Strike:   0,
+			Standard: 0,
+		},
+		Absorptions: output.Absorptions{
+			Magic:     0,
+			Lightning: 0,
+			Fire:      0,
+			Dark:      0,
+			Poison:    0,
+			Bleed:     0,
+			Petrify:   0,
+			Curse:     0,
+		},
+		Poise:  0,
+		Weight: 0,
+	}
+	noGauntlets = output.Armor{
+		Name: "No Gauntlets",
+		Defenses: output.Defenses{
+			Slash:    0,
+			Thrust:   0,
+			Strike:   0,
+			Standard: 0,
+		},
+		Absorptions: output.Absorptions{
+			Magic:     0,
+			Lightning: 0,
+			Fire:      0,
+			Dark:      0,
+			Poison:    0,
+			Bleed:     0,
+			Petrify:   0,
+			Curse:     0,
+		},
+		Poise:  0,
+		Weight: 0,
+	}
+	noLeggings = output.Armor{
+		Name: "No Leggings",
+		Defenses: output.Defenses{
+			Slash:    0,
+			Thrust:   0,
+			Strike:   0,
+			Standard: 0,
+		},
+		Absorptions: output.Absorptions{
+			Magic:     0,
+			Lightning: 0,
+			Fire:      0,
+			Dark:      0,
+			Poison:    0,
+			Bleed:     0,
+			Petrify:   0,
+			Curse:     0,
+		},
+		Poise:  0,
+		Weight: 0,
+	}
+	noRing = output.Ring{
+		Name: "No Ring",
+	}
+	// fists = output.Weapon{
+	// 	Name: "Fists",
+	// 	Requirements: output.ScalingStats{
+	// 		Strength:     0,
+	// 		Dexterity:    0,
+	// 		Intelligence: 0,
+	// 		Faith:        0,
+	// 	},
+	// 	Category: "fists",
+	// 	Paired:   false,
+	// 	Infusions: map[string]output.Infusion{
+	// 		"standard": {
+	// 			Name: "Standard",
+	// 			BaseDamage: output.Damage{
+	// 				Physical:  10,
+	// 				Magic:     10,
+	// 				Lightning: 10,
+	// 				Fire:      10,
+	// 				Dark:      10,
+	// 				Poison:    800,
+	// 				Bleed:     800,
+	// 				Petrify:   0,
+	// 				Curse:     0,
+	// 			},
+	// 			Scaling: output.ScalingStats{
+	// 				// TODO
+	// 				Strength:     0,
+	// 				Dexterity:    0,
+	// 				Intelligence: 0,
+	// 				Faith:        0,
+	// 			},
+	// 		},
+	// 	},
+	// }
+)
---
+++ pkg/transform/enums.go
@@ -0,0 +1,47 @@
+package transform
+
+// Weapon infusions
+const (
+	infusionStandard = iota
+	infusionLightning
+	infusionMagic
+	infusionFire
+	infusionDark
+	infusionPoison
+	infusionBleed
+	infusionRaw
+	infusionEnchanted
+	infusionMundane
+	infusionBoss
+	infusionSpecial
+)
+
+// Weapon categories
+const (
+	dagger = iota
+	straightSword
+	greatsword
+	ultraGreatsword
+	curvedSword
+	katana
+	curvedGreatsword
+	piercingSword
+	axe
+	greatAxe
+	hammer
+	greatHammer
+	fist
+	claw
+	spear
+	halberd
+	lance
+	reaper
+	twinblade
+	whip
+	bow
+	greatbow
+	crossbow
+	staff
+	pyromancyFlame
+	chime
+)
