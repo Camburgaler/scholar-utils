14cb5dcffc111dda0dd84edc8f6b6f1399b121c7
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-07-09 09:29:48 -0500

    added Identifier and DS2EMEVD structs

--- pkg/data/emevd/parse/types.go
+++ pkg/data/emevd/parse/types.go
@@ -3,6 +3,10 @@ package parse
 import "github.com/Camburgaler/scholar-utils/pkg/data/params/id"
 
 type (
+	Identifier struct {
+		Parts []string
+	}
+
 	Statement struct {
 		Name string
 		Args []string
@@ -11,5 +15,10 @@ type (
 	Event struct {
 		ID         id.ID
 		Statements []Statement
+	}
+	DS2EMEVD struct {
+		SpEffectArmor  []Event
+		SpEffectRing   []Event
+		SpEffectWeapon []Event
 	}
 )
